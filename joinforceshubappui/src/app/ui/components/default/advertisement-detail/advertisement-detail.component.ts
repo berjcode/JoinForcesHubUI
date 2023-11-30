cae9d14ddec0641c3a149fd5025678fc46b1e161
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-11-30 16:33:08 +0300

    The layout structure was established and a ready theme was added

---
+++ joinforceshubappui/src/app/ui/components/default/advertisement-detail/advertisement-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-advertisement-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './advertisement-detail.component.html',
+  styleUrl: './advertisement-detail.component.css'
+})
+export class AdvertisementDetailComponent {
+
+}
--- joinforceshubappui/src/main.ts
+++ joinforceshubappui/src/main.ts
@@ -1,6 +1,34 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      RouterModule.forRoot([
+        {
+          path: '',
+          loadComponent: () =>
+            import('./app/ui/components/layouts/layouts.component').then(
+              (c) => c.LayoutsComponent
+            ),
+          children: [
+            {
+              path: '',
+              loadComponent: () =>
+                import('./app/ui/components/default/home/home.component').then(
+                  (c) => c.HomeComponent
+                )
+            },
+          ],
+        },
+        {
+          path: 'login',
+          loadComponent :() => import('./app/ui/components/auth/login/login.component').then(c=> c.LoginComponent)
+        }
+      ])
+    ),
+  ],
+});
